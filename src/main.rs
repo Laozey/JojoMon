9f9a0f2503bc530b0da198a1304e0cd7c747c4f5
Bibi210 <brahimadibassi@gmail.com>
2020-12-13 02:19:31 +0100

    Debut du projet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-use amethyst;
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+use ggez::*;
+use legion::*;
+
+mod stand_data;
+use stand_data::*;
+
+//*Player 1 select attack
+//*Player 2 select attack
+//* Process Effect and Attack Order based on Player speed
+//* Go next turn
 
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::default();
+    let dio = StandStats::dio();
+    let jotaro = StandStats::jotaro();
+   
+    let perso1 = world.push((jotaro,));
+    let perso2 = world.push((dio,));
+    
 }
---
+++ src/stand_data.rs
@@ -0,0 +1,82 @@
+#[derive(Debug)]
+pub enum Effets {
+    Stun(u32),
+    Regeneration(u32, u32),
+    LostAttack(u32, u32),
+    LostSpeed(u32, u32),
+    Lostprecision(u32, u32),
+    LostEscape(u32, u32),
+}
+
+//? A voir si on l'ajoute
+pub enum Passifs {
+    //* Dio comes back to life
+    UnDead,
+
+    //* La puissance Jotaro de augemente a mesure qu'il s'approche de son adversaire
+    //* "+ de tour = + de stats"
+    BigBalls,
+}
+
+pub enum Attacks {
+    //? Commun capacity
+    //* Stun l'adversaire pendant x tour
+    Zawarudo,
+    //* Une attaque special "déluge de coups"
+    BeatUp(u32),
+
+    //? Dio capacity
+    Muda(u32),
+    RoadRoller(u32),
+    Charisme, //* Dio tape une pose
+    //* Dio est beau donc toute attaque est annulé
+
+    //? Jotaro capacity
+    //* Jotaro jure sur la tête de sa mére
+    //* Met le doute dans le coeur de son adversaire il perds bcp de precision pour 2 tour
+    //* Dans certain cas l'adversaire meurt
+    MotherSoul,
+    Ora(u32),
+}
+
+#[derive(Debug)]
+pub struct StandStats {
+    hp: i32,
+    speed: u32,
+    attack: u32,
+    accuracy: u32,
+    escape: u32,
+    status: Vec<Effets>,
+}
+
+impl StandStats {
+    fn new(hp: i32, speed: u32, attack: u32, accuracy: u32, escape: u32) -> Self {
+        StandStats {
+            hp: hp,
+            speed: speed,
+            attack: attack,
+            accuracy: accuracy,
+            escape: escape,
+            status: Vec::new(),
+        }
+    }
+    pub fn dio() -> Self {
+        return StandStats::new(200,10,15,85,40)
+    }
+    pub fn jotaro() -> Self {
+        return StandStats::new(200,8,13,90,10)
+    }
+}
+
+impl Default for StandStats {
+    fn default() -> Self {
+        return StandStats {
+            hp: 100,
+            speed: 5,
+            attack: 10,
+            accuracy: 90,
+            escape: 10,
+            status: vec![],
+        };
+    }
+}
